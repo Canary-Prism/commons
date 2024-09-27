424487894f51c6a2e848a93b45b00bc1405b6a6a
Canary-Prism <canaryprsn@gmail.com>
2024-09-28 01:11:07 +0800

    more javadocs :3

--- commons-cursed/src/main/java/canaryprism/commons/cursed/CursedException.java
+++ commons-cursed/src/main/java/canaryprism/commons/cursed/CursedException.java
@@ -1,4 +1,12 @@
 package canaryprism.commons.cursed;
+
+/**
+ * <p>
+ * A runtime exception that is thrown when the 
+ * {@link CursedUtils#catchExceptions()} or 
+ * {@link CursedUtils#catchExceptionsWithParams()} method fails
+ * </p>
+ */
 public class CursedException extends RuntimeException {
     public CursedException(String message) {
         super(message);
